8fe577dd1c54e054046eda286e99709985a49677
Kittipat Tachamanokul <Kittipat_t@cmu.ac.th>
2024-02-29 17:18:27 +0700

    change pic

--- deploy_iris.py
+++ deploy_iris.py
@@ -16,7 +16,7 @@ def predict_iris_variety(sepal_lenght,sepal_width,petal_lenght,petal_width):
 
 def Input_Output():
     st.title("Iris Variety Prediction")
-    st.image("img/Iris-Dataset-Classification.png", width=600)
+    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw6jv1EQ73VX_ZyArbTiOdEmW2T6ZZojbj6fIofPbmaA&s", width=600)
     
     st.markdown("You are using Streamlit...",unsafe_allow_html=True)
     sepal_lenght = st.text_input("Enter Sepal Lenght" , ".")
